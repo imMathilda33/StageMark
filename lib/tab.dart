5976d4959f724036cbf7f3d66b968cf69d332f83
Xiangrui Meng <mathilda332023@outlook.com>
2024-04-03 17:05:14 +0100

    add style

--- lib/tab.dart
+++ lib/tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'calendar.dart';
 import 'event.dart';
 import 'login.dart';
@@ -13,13 +14,12 @@ class MyTabbedPage extends StatefulWidget {
 
 class _MyTabbedPageState extends State<MyTabbedPage> with SingleTickerProviderStateMixin {
   late TabController _tabController;
-  bool isLoggedIn = false; 
+  bool isLoggedIn = false;
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 4, vsync: this);
-    
     FirebaseAuth.instance.authStateChanges().listen((User? user) {
       setState(() {
         isLoggedIn = user != null; 
@@ -34,45 +34,51 @@ class _MyTabbedPageState extends State<MyTabbedPage> with SingleTickerProviderSt
   }
 
   Widget _getTabContent() {
-    return isLoggedIn ? UserPage() : LoginRegisterPage(onLoginSuccess: () {
+    return isLoggedIn ? UserPage(tabController: _tabController) : LoginRegisterPage(onLoginSuccess: () {
       setState(() {
         isLoggedIn = true; 
       });
-    });
+    }, tabController: _tabController,);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
+    // Transparent Navigation Bars
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        systemNavigationBarColor:  Theme.of(context).colorScheme.background,
+        statusBarColor: Theme.of(context).colorScheme.background,
       ),
+    );
+
+    return Scaffold(
+      appBar: null, // Disable the app bar
+      // extendBody: true, // Extend the body behind the bottom navigation bar
+      // extendBodyBehindAppBar: true,
       body: SafeArea(
         child: TabBarView(
           controller: _tabController,
           children: [
             Calendar(),  
-            Event(),     
+            Event(tabController: _tabController,),
             FunctionPage(),
-            isLoggedIn ? UserPage() : LoginRegisterPage(),  
+            isLoggedIn ? UserPage(tabController: _tabController) : LoginRegisterPage(tabController: _tabController,),
           ],
         ),
       ),
       bottomNavigationBar: Container(
-        color: Colors.white,
-        padding: EdgeInsets.only(bottom: 16),
+        color:  Colors.white,
+        // padding: EdgeInsets.only(bottom: 0),
         child: TabBar(
           controller: _tabController,
           unselectedLabelColor: Color.fromARGB(255, 150, 150, 150),
           indicatorColor: Color.fromRGBO(247, 157, 138, 1),
-          labelColor: Color.fromRGBO(247, 157, 138, 1),
+          labelColor:  Color.fromRGBO(247, 157, 138, 1),
           tabs: [
-            Tab(icon: Icon(Icons.calendar_month)),  
+            Tab(icon: Icon(Icons.calendar_month)),
             Tab(icon: Icon(Icons.playlist_add)),    
             Tab(icon: Icon(Icons.folder_special)),  
-            Tab(icon: Icon(Icons.edit_note)),       
+            Tab(icon: Icon(Icons.person)),
           ],
         ),
       ),
