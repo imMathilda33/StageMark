48c1b6a780985a8e5b4cc84c3c4f7eac7073583e
Xiangrui Meng <mathilda332023@outlook.com>
2024-04-16 15:46:56 +0100

    edit workflow

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         tabBarTheme: const TabBarTheme(dividerColor: Colors.transparent),
         splashFactory: NoSplash.splashFactory,
